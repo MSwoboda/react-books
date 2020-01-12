1f70ec361209267ed1664d18f16a4d87cab68bad
Michał Swoboda <michal.swoboda@outlook.com>
2020-01-12 18:33:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,6 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
 
 app.listen(PORT, () => console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`));
-// heroku config:set MONGOLAB_URI=mongodb://googlebooks:qMPBJsE!9t6Ddnz@ds031741.mlab.com:31741/heroku_p595vcpv/googlebooks
-
-
-heroku config:set MONGOLAB_URI=mongodb://googlebooks:qMPBJsE!9t6Ddnz@ds031741.mlab.com:31741/heroku_p595vcpv
-// mongodb://<dbuser>:<dbpassword>@ds031741.mlab.com:31741/heroku_p595vcpv   ssssss  "mongodb://googlebooks:qMPBJsE!9t6Ddnz@ds031741.mlab.com:31741/heroku_p595vcpv/googlebooks"
